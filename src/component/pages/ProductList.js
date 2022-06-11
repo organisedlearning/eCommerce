8ab70068619f87a8ac58edf788037603a68b6287
rajukumarazh <rajukumarazh@gmail.com>
2022-06-11 17:11:00 +0530

    product list generated

--- src/component/pages/ProductList.js
+++ src/component/pages/ProductList.js
@@ -1,6 +1,34 @@
 import React from "react";
-
+import { useEffect } from "react";
+import axios from "axios";
+import { FaStar } from "react-icons/fa";
+// import { setProducts } from "../../redux/Action/productActions";
+import { setProducts } from "../../redux/actions/productActions";
+import { useDispatch, useSelector } from "react-redux";
 function ProductList() {
+	const products = useSelector((state) => {
+		return state.allProducts.products;
+	});
+	console.log("kkkkkk", products);
+	const dispatch = useDispatch(setProducts);
+	const fetchProducts = async () => {
+		const resp = await axios
+			.get(
+				"https://600c30e638fd25001702cf7e.mockapi.io/api/v1/products",
+			)
+			.catch((err) => {
+				console.error(
+					`Error in fetchProducts API call with ${err}`,
+				);
+			});
+		console.log("dkkkkk", resp.data);
+		dispatch(setProducts(resp.data));
+	};
+
+	useEffect(() => {
+		fetchProducts();
+	}, []);
+
 	return (
 		//  <!--
 		//   This example requires Tailwind CSS v2.0+
@@ -25,66 +53,61 @@ function ProductList() {
 				</h2>
 
 				<div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-					<div class="group relative">
-						<div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-							<img
-								src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
-								alt="Front of men&#039;s Basic Tee in black."
-								class="w-full h-full object-center object-cover lg:w-full lg:h-full"
-							/>
-						</div>
-						<div class="mt-4 flex justify-between">
-							<div>
-								<h3 class="text-sm text-gray-700">
-									<a href="#">
-										<span
-											aria-hidden="true"
-											class="absolute inset-0"
-										></span>
-										Basic
-										Tee
-									</a>
-								</h3>
-								<p class="mt-1 text-sm text-gray-500">
-									Black
-								</p>
-							</div>
-							<p class="text-sm font-medium text-gray-900">
-								$35
-							</p>
-						</div>
-					</div>
-
-					{/* <!-- More products... --> */}
-					<div class="group relative">
-						<div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-							<img
-								src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
-								alt="Front of men&#039;s Basic Tee in black."
-								class="w-full h-full object-center object-cover lg:w-full lg:h-full"
-							/>
-						</div>
-						<div class="mt-4 flex justify-between">
-							<div>
-								<h3 class="text-sm text-gray-700">
-									<a href="#">
-										<span
-											aria-hidden="true"
-											class="absolute inset-0"
-										></span>
-										Basic
-										Tee
-									</a>
-								</h3>
-								<p class="mt-1 text-sm text-gray-500">
-									Black
-								</p>
-							</div>
-							<p class="text-sm font-medium text-gray-900">
-								$35
-							</p>
-						</div>
-					</div>
+					{products !== undefined &&
+						products.map(
+							(
+								single,
+							) => {
+								return (
+									<div class="group relative">
+										<div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+											<img
+												src={
+													single.image
+												}
+											/>
+										</div>
+										<div class="mt-4 flex justify-between">
+											<div>
+												<h3 class="text-sm text-gray-700">
+													<a href="#">
+														<span
+															aria-hidden="true"
+															class="absolute inset-0"
+														></span>
+														{
+															single.name
+														}
+													</a>
+												</h3>
+												<p class="mt-1 text-sm text-gray-500"></p>
+											</div>
+											<p class="text-sm font-medium text-gray-900">
+												{
+													single.price
+												}
+											</p>
+											<span className="text-yellow-400 flex gap-2">
+												<FaStar
+													size={
+														20
+													}
+												/>
+												{
+													single.stars
+												}
+											</span>
+										</div>
+										<button className="w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
+											{" "}
+											Add
+											To
+											Cart
+										</button>
+									</div>
+								);
+							},
+						)}
 				</div>
 			</div>
 		</div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+	<BrowserRouter>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</BrowserRouter>,
+
+	document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
